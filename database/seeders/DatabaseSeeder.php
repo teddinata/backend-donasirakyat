8e472e1a262c664fdab0349acc60d721e1d97a08
teddinata <teddinata@users.noreply.github.com>
2022-09-05 19:57:30 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        // user seeder
+        $this->call(UserSeeder::class);
     }
 }
